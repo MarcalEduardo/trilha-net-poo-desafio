1686f8e36f46ff8154f1e5f849ffe33dd8b78a15
Eduardo Marcal <emclemente@outlook.com.br>
2024-05-09 18:47:39 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("SmartPhone Nokia");
+
+        Nokia c1 = new Nokia("1", "Nokia", "122", 64);
+        c1.Ligar();
+        c1.InstalarAplicativo("Google Maps");
+
+        Console.WriteLine("\n");
+
+
+         Console.WriteLine("SmartPhone Iphone");
+
+        Iphone c2 = new Iphone("2", "Iphone", "222", 128);
+        c2.Ligar();
+        c2.InstalarAplicativo("Instagram");
+
+       
+    }
+    
+}
